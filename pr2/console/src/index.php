25623b7785182c0c9b09221e65609c9452109c0c
vitalir2 <vitalyi100@mail.ru>
2022-10-06 21:43:43 +0300

    Improve console webpage in pr2, 3rd

--- pr2/console/src/index.php
+++ pr2/console/src/index.php
@@ -1,7 +1,25 @@
-<?php
-$command = $_POST['inp'];
+<html lang="en">
+<head>
+    <title>Hello console page</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input name="inp" />
+        <input type="submit" name="submit" value="submit" />
+    </form>
+    <?php
+    $whitelist = array("ls", "cat", "echo", "whoami", "head", "tail");
+    $command = $_POST['inp'];
 
-echo $command, '<br/>';
-echo shell_exec($command);
-?>
+    echo $command, "<br></br>";
+    $command_name = strtok($command, " ");
+    if ($command_name !== false && in_array($command_name, $whitelist)) {
+      echo shell_exec($command);
+    } else {
+      echo "Not permitted command";
+    }
+    ?>
+</body>
+</html>
 

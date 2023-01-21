6f76fc44220ab7f7b728b811d4c8dd90e5462d70
vitalir2 <vitalyi100@mail.ru>
2023-01-21 15:41:08 +0300

    Add final blanket pages for the 3rd practice

--- pr3/src/dynamic/src/registration.php
+++ pr3/src/dynamic/src/registration.php
@@ -3,6 +3,13 @@
     <title>Registration</title>
   </head>
   <body>
-  <!-- TODO -->
+  <form>
+      <label for="name"
+  </form>
+    <?php
+    function encode_password($password) {
+        return '{SHA}' . base64_encode(sha1($password, TRUE));
+    }
+     ?>
   </body>
 </html>
